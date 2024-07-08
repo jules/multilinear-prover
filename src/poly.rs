21d7cd1455c1fa7a26ecfad1f58fab0805e7a23d
Jules de Smit <julesdesmit@gmail.com>
2024-07-08 11:47:09 +0200

    more poly func

--- src/poly.rs
+++ src/poly.rs
@@ -14,8 +14,30 @@ impl<F: Field> MultilinearPoly<F> {
         }
     }
 
+    pub fn num_vars(&self) -> usize {
+        self.num_vars
+    }
+
+    /// Returns an evaluation on the hypercube.
     pub fn eval(&self, index: usize) -> F {
         debug_assert!(index < self.evals.len());
         self.evals[index]
+    }
+
+    /// Sets the first variable of the polynomial to `point`. Used for fixing sumcheck challenges
+    /// into the polynomial.
+    pub fn fix_variable(&self, point: F) -> Self {
+        let mut new = MultilinearPoly {
+            num_vars: self.num_vars - 1,
+            evals: Vec::with_capacity(1 << (self.num_vars - 1)),
+        };
+
+        // TODO: par
+        for i in 0..(1 << (new.num_vars)) {
+            new.evals
+                .push(self.evals[i] + (self.evals[(i << 1) + 1] - self.evals[i << 1]) * point);
+        }
+
+        new
     }
 }
