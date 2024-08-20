dcc384ca6242cf7ce6205517cd5b587244cdcd60
Jules de Smit <julesdesmit@gmail.com>
2024-08-20 07:06:57 +0200

    fix and extend transcript

--- src/transcript.rs
+++ src/transcript.rs
@@ -8,8 +8,10 @@ pub trait Transcript<F: Field>: Send + Sync {
     fn draw_bits(&mut self, bits: usize) -> usize;
     fn observe_witness(&mut self, witness: F);
     fn observe_witnesses(&mut self, witness: &[F]);
+    fn observe_hashes(&mut self, hashes: &[[u8; 32]]);
 }
 
+#[derive(Default)]
 pub struct Blake2sTranscript<F: Field> {
     hasher: Blake2s256,
     state: Vec<u8>,
@@ -19,9 +21,9 @@ pub struct Blake2sTranscript<F: Field> {
 impl<F: Field> Blake2sTranscript<F> {
     // We wrap this into a convenient method since we need to do some state trickery.
     fn output_hash(&mut self) -> [u8; 32] {
+        self.hasher.update(self.state.clone());
         let output = <[u8; 32]>::from(self.hasher.finalize_reset());
         self.state.extend(output.to_vec());
-        self.hasher.update(self.state.clone());
         output
     }
 }
@@ -33,7 +35,7 @@ where
     fn draw_challenge(&mut self) -> F {
         let output = self.output_hash();
         let mut arr = [0u8; 8];
-        arr.copy_from_slice(&output[..F::NUM_BYTES_IN_REPR]);
+        arr[..F::NUM_BYTES_IN_REPR].copy_from_slice(&output[..F::NUM_BYTES_IN_REPR]);
         let n = usize::from_le_bytes(arr);
         F::from_usize(n)
     }
@@ -44,9 +46,10 @@ where
         let output = self.output_hash();
         let els = output
             .chunks(F::NUM_BYTES_IN_REPR)
+            .take(E::DEGREE)
             .map(|chunk| {
                 let mut arr = [0u8; 8];
-                arr.copy_from_slice(&chunk);
+                arr[..F::NUM_BYTES_IN_REPR].copy_from_slice(&chunk);
                 let n = usize::from_le_bytes(arr);
                 F::from_usize(n)
             })
@@ -80,14 +83,20 @@ where
     fn observe_witness(&mut self, witness: F) {
         let bytes = witness.to_le_bytes().to_vec();
         self.state.extend(bytes.clone());
-        self.hasher.update(bytes);
     }
 
     fn observe_witnesses(&mut self, witness: &[F]) {
+        self.state.reserve(witness.len() * F::NUM_BYTES_IN_REPR);
         for w in witness {
             let bytes = w.to_le_bytes().to_vec();
             self.state.extend(bytes.clone());
-            self.hasher.update(bytes);
+        }
+    }
+
+    fn observe_hashes(&mut self, hashes: &[[u8; 32]]) {
+        self.state.reserve(hashes.len() * 32);
+        for h in hashes {
+            self.state.extend(h.clone());
         }
     }
 }
