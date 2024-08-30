9d1ba69ef414acdc9805ad72155e3c6856dd386c
Jules de Smit <julesdesmit@gmail.com>
2024-08-30 08:00:32 +0200

    use parallelization and batch inversion for logup helpers

--- src/iop/logup.rs
+++ src/iop/logup.rs
@@ -1,6 +1,7 @@
 //! Tooling for LogUp. https://eprint.iacr.org/2022/1530.pdf
 
 use crate::{field::Field, polynomial::MultilinearExtension};
+use rayon::prelude::*;
 
 /// Compute multiplicities of table values over all columns, assuming the table is injective.
 pub fn compute_multiplicities<F: Field>(
@@ -33,11 +34,7 @@ pub fn compute_helper_columns<F: Field>(
     let mut helper_columns = Vec::with_capacity(x_plus_columns.len() + 1);
 
     // First column: m / (x + t).
-    let inverses = x_plus_table
-        .evals
-        .iter()
-        .map(|value| value.inverse().unwrap())
-        .collect::<Vec<F>>();
+    let inverses = F::batch_inverse(&x_plus_table.evals).expect("table should be batch invertable");
 
     helper_columns.push(MultilinearExtension::new(
         multiplicities
@@ -53,19 +50,19 @@ pub fn compute_helper_columns<F: Field>(
     ));
 
     // Rest is just the negative reciprocal of x + column.
-    x_plus_columns.iter().for_each(|column| {
-        helper_columns.push(MultilinearExtension::new(
-            column
-                .evals
-                .iter()
-                .map(|value| {
-                    let mut inv = value.inverse().unwrap();
-                    inv.negate();
-                    inv
-                })
-                .collect::<Vec<F>>(),
-        ));
-    });
+    helper_columns.extend(
+        x_plus_columns
+            .par_iter()
+            .map(|column| {
+                let mut inverses =
+                    F::batch_inverse(&column.evals).expect("column should be batch invertable");
+                inverses.iter_mut().for_each(|value| {
+                    value.negate();
+                });
+                MultilinearExtension::new(inverses)
+            })
+            .collect::<Vec<MultilinearExtension<F>>>(),
+    );
 
     helper_columns
 }
